be289dbf3f4242a2acb3960f2023159203c76c64
pachara.tho@1moby.com <pachara.tho@1moby.com>
2019-05-08 15:51:36 +0700

    [Edit]-Description package.

--- client/client.go
+++ client/client.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-// Movider REST API client for Go. API support for SMS, Verify, Acknowledge Verify and Cancel Verify.
+// Movider API client for Go. API support for SMS, Verify, Acknowledge Verify and Cancel Verify.
 package client
 
 import (
